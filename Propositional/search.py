7690c35438746bb7ca30f840e123c323ab91d2cd
Keith Allatt <keith.allatt@gmail.com>
2021-03-02 18:50:57 -0500

    Made BFS derivation search prune unnecessary arguments.

--- Propositional/search.py
+++ Propositional/search.py
@@ -8,7 +8,6 @@ class BFSDirectDerivation:
         self.consequence = goal_proposition
 
     def search(self):
-
         if type(self.consequence) is LogicalImplies:
             d = ConditionalDerivation(self.axioms, self.consequence, [])
         else:
@@ -18,62 +17,61 @@ class BFSDirectDerivation:
             raise LogicalException("Cannot search for proof in invalid argument." +
                                    "Counter example: " + str(d.get_counter_example()))
 
-        environment = {ax: [] for ax in d.axioms}
-        environment.update({ax: [] for ax in d.assumptions})
+        def find_path():
+            environment = {ax: [] for ax in d.axioms}
+            environment.update({ax: [] for ax in d.assumptions})
 
-        while True:
-            new_environment = {}
-            max_depth = max([arg.num_inputs for arg in DERIVATIONS])
+            while True:
+                new_environment = {}
+                max_depth = max([arg.num_inputs for arg in DERIVATIONS])
 
-            def gen_env_tuples(depth: int):
-                if depth == 1:
-                    return [(env,) for env in environment.items()]
+                def gen_env_tuples(depth: int):
+                    if depth == 1:
+                        return [(env,) for env in environment.items()]
 
-                return [
-                    (env, *tup) for env in environment.items() for tup in gen_env_tuples(depth-1)
-                ] + gen_env_tuples(depth-1)
+                    return [(env, *tup) for env in environment.items()
+                            for tup in gen_env_tuples(depth-1)] + gen_env_tuples(depth-1)
 
-            env_tuples = gen_env_tuples(max_depth)
+                env_tuples = gen_env_tuples(max_depth)
+                for argument in DERIVATIONS:
+                    for env_tup in env_tuples:
+                        con, _path = [env[0] for env in env_tup], [env[1] for env in env_tup]
+                        if argument.num_inputs != len(con):
+                            # should not work if not right number of parameters
+                            continue
 
-            for argument in DERIVATIONS:
-                for env_tup in env_tuples:
-                    con, path = [env[0] for env in env_tup], [env[1] for env in env_tup]
+                        arg = argument(*con)
 
-                    if argument.num_inputs != len(con):
-                        # should not work if not right number of parameters
-                        continue
+                        try:
+                            path_augmented = sum(_path, []) + [arg]
+                            application = arg.apply()
+                            # if apply works
+                            if application == d.consequence:
+                                return path_augmented
+                            # if distinct from all before it.
+                            for e_c in environment.items():
+                                if e_c[0] == application:
+                                    continue
+                            for e_c in new_environment.items():
+                                if e_c[0] == application:
+                                    continue
+                            # add to environment
+                            new_environment.update({application: path_augmented})
+                        except LogicalException:
+                            pass
+                if new_environment == {}:
+                    return []
+                environment.update(new_environment)
 
-                    arg = argument(*con)
+        path = find_path()
+        # remove unnecessary elements.
+        for i in range(len(path)-1, -1, -1):
+            new_path = path[:i] + path[i+1:]
+            d.derivation = new_path
+            if "Q.E.D." in d.verify():
+                path = new_path
 
-                    try:
-
-                        new_path = sum(path, []) + [arg]
-                        application = arg.apply()
-
-                        if application == d.consequence:
-                            return new_path
-
-                        # if apply works
-
-                        # if distinct from all before it.
-                        for e_c in environment.items():
-                            if e_c[0] == application:
-                                continue
-                        for e_c in new_environment.items():
-                            if e_c[0] == application:
-                                continue
-
-                        # add to environment
-
-                        new_environment.update({application: new_path})
-
-                    except LogicalException:
-                        pass
-
-            if new_environment == {}:
-                return []
-
-            environment.update(new_environment)
+        return path
 
 
 if __name__ == '__main__':
